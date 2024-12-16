b370d7eb40258ed801d4eadd7ec973a35de93d5d
SystemX <k.yanushevskiy@gmail.com>
2024-12-16 20:23:15 +0400

    raz'eb

--- src/main/java/org/example/authFilter/AuthFilter.java
+++ src/main/java/org/example/authFilter/AuthFilter.java
@@ -1,34 +1,23 @@
-package org.example.authFilter;
-
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-public class AuthFilter implements Filter {
-
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        // TODO need check can i add here smthg for load every time buy loading every servlet.
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-        Object user = request.getSession().getAttribute("user");
-
-        if (user == null) {
-            response.sendRedirect("/login");
-        }else {
-            filterChain.doFilter(request, response);
-        }
-
-    }
-
-
-}
+//package org.example.authFilter;
+//
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//
+//public class AuthFilter implements Filter {
+//
+//
+//    @Override
+//    public void init(FilterConfig filterConfig) throws ServletException {
+//        // TODO need check can i add here smthg for load every time buy loading every servlet.
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+//
+//
+//    }
+//}
---
+++ src/main/java/org/example/container/DependencyContainer.java
@@ -0,0 +1,2 @@
+package org.example.container;public class DependencyContainer {
+}
